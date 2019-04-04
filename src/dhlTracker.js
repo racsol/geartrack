ba63570f24379ccc34560b22ebbcf7188307b730
Silva, Carlos <Carlos.Silva@adtalem.com>
2019-04-04 09:20:23 +0100

    Changed default language to english for dhl

--- src/dhlTracker.js
+++ src/dhlTracker.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const request = require('requestretry')
-const utils = require('./utils')
-const moment = require('moment-timezone')
-const zone = "Europe/Berlin"
+const request = require("requestretry");
+const utils = require("./utils");
+const moment = require("moment-timezone");
+const zone = "Europe/Berlin";
 
-const URL = 'http://www.dhl.com/shipmentTracking?AWB={{id}}&languageCode=pt'
+const URL = "http://www.dhl.com/shipmentTracking?AWB={{id}}&languageCode=en";
 
-const exportModule = {}
+const exportModule = {};
 
 /**
  * Get DHL tracker info
@@ -17,9 +17,9 @@ const exportModule = {}
  * @param id
  * @param callback(Error, DHLTrackerInfo)
  */
-exportModule.getInfo = function (id, callback) {
-    obtainInfo(id, URL.replace("{{id}}", id), callback)
-}
+exportModule.getInfo = function(id, callback) {
+  obtainInfo(id, URL.replace("{{id}}", id), callback);
+};
 
 /**
  * Get info from tracker request
@@ -29,31 +29,34 @@ exportModule.getInfo = function (id, callback) {
  * @param cb
  */
 function obtainInfo(id, action, cb) {
-    request.get({
-        url: action,
-        timeout: 10000,
-        maxAttempts: 2
-    }, function (error, response, body) {
-        if (error || response.statusCode != 200) {
-            cb(utils.errorDown())
-            return
-        }
+  request.get(
+    {
+      url: action,
+      timeout: 10000,
+      maxAttempts: 2
+    },
+    function(error, response, body) {
+      if (error || response.statusCode != 200) {
+        cb(utils.errorDown());
+        return;
+      }
 
-        const data = JSON.parse(body)
-        if (data.errors != undefined) {
-            cb(utils.errorNoData())
-            return
-        }
+      const data = JSON.parse(body);
+      if (data.errors != undefined) {
+        cb(utils.errorNoData());
+        return;
+      }
 
-        let entity = null
-        try {
-            entity = createTrackerEntity(data)
-        } catch (error) {
-            return cb(utils.errorParser(id, error.message))
-        }
+      let entity = null;
+      try {
+        entity = createTrackerEntity(data);
+      } catch (error) {
+        return cb(utils.errorParser(id, error.message));
+      }
 
-        if(entity != null) cb(null, entity)
-    })
+      if (entity != null) cb(null, entity);
+    }
+  );
 }
 
 /**
@@ -61,24 +64,23 @@ function obtainInfo(id, action, cb) {
  * @param html
  */
 function createTrackerEntity(data) {
+  let result = data.results[0];
 
-    let result = data.results[0]
-
-    return new TrackerInfo({
-        attempts: 1,
-        id: result.id,
-        transportType: result.type,
-        origin: result.origin.value,
-        destiny: result.destination.value,
-        states: result.checkpoints.map((elem) => {
-            let date = elem.date.trim() + " " + elem.time.trim()
-            return {
-                state: elem.description.trim(),
-                date: moment.tz(date, "dddd, MMMM DD, YYYY HH:mm", 'pt', zone).format(),
-                area: elem.location.trim()
-            }
-        })
+  return new TrackerInfo({
+    attempts: 1,
+    id: result.id,
+    transportType: result.type,
+    origin: result.origin.value,
+    destiny: result.destination.value,
+    states: result.checkpoints.map(elem => {
+      let date = elem.date.trim() + " " + elem.time.trim();
+      return {
+        state: elem.description.trim(),
+        date: moment.tz(date, "dddd, MMMM DD, YYYY HH:mm", "pt", zone).format(),
+        area: elem.location.trim()
+      };
     })
+  });
 }
 
 /*
@@ -87,17 +89,16 @@ function createTrackerEntity(data) {
  |--------------------------------------------------------------------------
  */
 function TrackerInfo(obj) {
-    this.attempts = obj.attempts
-    this.id = obj.id
-    this.deliveryDate = obj.deliveryDate
-    this.states = obj.states
-    this.origin = obj.origin,
-    this.destiny = obj.destiny
-    this.trackerWebsite = exportModule.getLink(this.id)
+  this.attempts = obj.attempts;
+  this.id = obj.id;
+  this.deliveryDate = obj.deliveryDate;
+  this.states = obj.states;
+  (this.origin = obj.origin), (this.destiny = obj.destiny);
+  this.trackerWebsite = exportModule.getLink(this.id);
 }
 
-exportModule.getLink = function (id) {
-    return "http://www.dhl.com/en/express/tracking.html?AWB=" + id
-}
+exportModule.getLink = function(id) {
+  return "http://www.dhl.com/en/express/tracking.html?AWB=" + id;
+};
 
-module.exports = exportModule
+module.exports = exportModule;
